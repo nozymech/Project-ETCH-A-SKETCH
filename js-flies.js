ed4cbb4003781170a3ce3b45163233ce6057ff52
nozymech <sjyang.pt@gmail.com>
2022-05-21 23:27:30 +0800

    Add tablet like UI

--- js-flies.js
+++ js-flies.js
@@ -1,35 +1,21 @@
-const container = document.getElementById("container")
-container.style.display = "flex";
-container.style.flexWrap = "wrap"
-container.style.width = "960px";
-let number = 0;
-let side = 0;
-const btn = document.getElementById("btn");
-
-btn.addEventListener("click",()=>{
-    number = prompt("How many number of squares per side do you need?");
-    let numSquare = 0
-    numSquare = number*number
-    for(i=0; i<(numSquare); i++){
-        const div = document.createElement('div')
-        div.setAttribute('class','square');
-        side = 960/number;
-        div.style.width=`${side}px`;
-        div.style.height=`${side}px`;
-        div.style.outline= 'solid';
-        container.append(div);
-
-        const square = document.querySelectorAll('div.square')
-        // get all div class name = square
-        square.forEach((square)=> {
-            // use forEach to loop all square & code want to excute to every square 
-            square.addEventListener("mouseover", ()=> {
-                square.style.backgroundColor = "yellow";
-            })
-        } )
-    }
-})
+let screen = document.getElementById("screen");
+let fragment = document.createDocumentFragment();
 
 
 
+for(i=0; i<1000; i++){
+  let square = document.createElement('div');
+  square.style.width = "30px"
+  square.style.height = "30px"
+  square.style.border = "solid"
+  square.setAttribute('class','square');
+  fragment.appendChild(square);
+};
+screen.append(fragment);
 
+const square = document.querySelectorAll("div.square")
+square.forEach((square)=> {
+    square.addEventListener("mouseover",()=>{
+        square.style.backgroundColor = "yellow";
+    })
+});
