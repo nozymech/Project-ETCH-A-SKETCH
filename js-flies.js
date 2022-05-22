6442fbe2173e513adefc61dc64786ad00a2d13d0
nozymech <sjyang.pt@gmail.com>
2022-05-22 10:52:38 +0800

    Add description

--- js-flies.js
+++ js-flies.js
@@ -2,10 +2,31 @@ let screen = document.getElementById("screen");
 let add = document.getElementById("add")
 let minus = document.getElementById("minus")
 let fragment = document.createDocumentFragment();
+// store square size
 let value = 0 ;
+// store number per side
 let number = 16 ;
 
-// click add screen: +1
+// initial screen: 16*16 square per side
+for(i=0; i<number*number; i++){
+    let square = document.createElement('div');
+    value = 580/number;
+    square.style.width = `${value}px`
+    square.style.height = `${value}px`
+    square.style.border = "solid"
+    square.setAttribute('class','square');
+    fragment.appendChild(square);
+};
+screen.append(fragment);
+
+let square = document.querySelectorAll("div.square")
+square.forEach((square)=> {
+    square.addEventListener("mouseover",()=>{
+        square.style.backgroundColor = "yellow";
+    })
+});
+
+// click add :  +1 square per side 
 add.addEventListener('click',()=>{
     number = number + 1;
     removeAllChildNodes(screen);
@@ -28,7 +49,7 @@ square.forEach((square)=> {
 });
 })
 
-// initial screen: 64*64 
+// click minus :  -1 square per side 
 minus.addEventListener('click',()=>{
     number = number - 1;
     removeAllChildNodes(screen);
@@ -51,25 +72,8 @@ square.forEach((square)=> {
 });
 })
 
-// initial screen: 64*64 
-for(i=0; i<number*number; i++){
-    let square = document.createElement('div');
-    value = 580/number;
-    square.style.width = `${value}px`
-    square.style.height = `${value}px`
-    square.style.border = "solid"
-    square.setAttribute('class','square');
-    fragment.appendChild(square);
-};
-screen.append(fragment);
 
-let square = document.querySelectorAll("div.square")
-square.forEach((square)=> {
-    square.addEventListener("mouseover",()=>{
-        square.style.backgroundColor = "yellow";
-    })
-});
-
+// remove all element
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
