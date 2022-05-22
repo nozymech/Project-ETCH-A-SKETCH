3efea7eef26a07dcd75c3b854e0ee738f5e18f04
nozymech <sjyang.pt@gmail.com>
2022-05-22 09:54:38 +0800

    adjust UI

--- js-flies.js
+++ js-flies.js
@@ -1,21 +1,38 @@
 let screen = document.getElementById("screen");
+let add = document.getElementById("add")
+let minus = document.getElementById("minus")
 let fragment = document.createDocumentFragment();
+let value = 15 ;
+
+add.addEventListener('click',()=>{
+    value = value + 1;
+    console.log(value);
+})
 
 
+minus.addEventListener('click',()=>{
+    value = value - 1;
+    console.log(value);
+})
 
-for(i=0; i<1000; i++){
-  let square = document.createElement('div');
-  square.style.width = "30px"
-  square.style.height = "30px"
-  square.style.border = "solid"
-  square.setAttribute('class','square');
-  fragment.appendChild(square);
+for(i=0; i<3000; i++){
+    let square = document.createElement('div');
+    square.style.width = `${value}px`
+    square.style.height = `${value}px`
+    square.style.border = "solid"
+    square.setAttribute('class','square');
+    fragment.appendChild(square);
 };
 screen.append(fragment);
 
-const square = document.querySelectorAll("div.square")
+let square = document.querySelectorAll("div.square")
 square.forEach((square)=> {
     square.addEventListener("mouseover",()=>{
         square.style.backgroundColor = "yellow";
     })
 });
+
+
+
+
+
