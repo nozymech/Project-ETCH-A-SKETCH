bdd3aa992968ddcd606a3e2ddbde08d1f77b4ac5
nozymech <sjyang.pt@gmail.com>
2022-05-22 10:02:11 +0800

    Change square size

--- js-flies.js
+++ js-flies.js
@@ -6,13 +6,31 @@ let value = 15 ;
 
 add.addEventListener('click',()=>{
     value = value + 1;
-    console.log(value);
+    removeAllChildNodes(screen);
+    for(i=0; i<3000; i++){
+        let square = document.createElement('div');
+        square.style.width = `${value}px`
+        square.style.height = `${value}px`
+        square.style.border = "solid"
+        square.setAttribute('class','square');
+        fragment.appendChild(square);
+    };
+    screen.append(fragment);
 })
 
 
 minus.addEventListener('click',()=>{
     value = value - 1;
-    console.log(value);
+    removeAllChildNodes(screen);
+    for(i=0; i<3000; i++){
+        let square = document.createElement('div');
+        square.style.width = `${value}px`
+        square.style.height = `${value}px`
+        square.style.border = "solid"
+        square.setAttribute('class','square');
+        fragment.appendChild(square);
+    };
+    screen.append(fragment);
 })
 
 for(i=0; i<3000; i++){
@@ -31,6 +49,12 @@ square.forEach((square)=> {
         square.style.backgroundColor = "yellow";
     })
 });
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
 
 
